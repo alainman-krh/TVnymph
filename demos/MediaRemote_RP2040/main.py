13d0390a7b2fe1ff003fc51f7e71dc10f8818f47
Alain <alainman.krh@gmail.com>
2024-10-19 17:46:53 -0400

    Reorg/add "SEND_SKIP_IF_FFREW_ONLY".

--- demos/MediaRemote_RP2040/main.py
+++ demos/MediaRemote_RP2040/main.py
@@ -17,7 +17,8 @@ import board
 #=Option slect
 #===============================================================================
 SEND_CONSUMERCONTROL_ONLY = False #Support basic media keys only (remote will not "type text")
-USE_NUMPAD = False #Send numpad keys vs standard numbers/enter key (SEND_CONSUMERCONTROL_ONLY must =False)
+SEND_SKIP_IF_FFREW_ONLY = True #Send "skip" keys on remotes that only support FF/RW (no proper skip buttons)
+SEND_NUMPAD = True #Send numpad keys vs standard numbers/enter key (SEND_CONSUMERCONTROL_ONLY must =False)
 
 
 #=Platform/build-dependent config (Raspberry Pi Pico RP2040)
@@ -26,47 +27,72 @@ rx_pin = board.GP28
 #Not likely to want "extras" to be sent to your PC (numeric values + arrows):
 
 
+#=Base keymap
+#===============================================================================
+FRAME_FORWARD = 0xC6
+KEYMAP = {
+    "PLAY": KeysCC(CCC.PLAY_PAUSE)        , "PAUSE": KeysCC(CCC.PLAY_PAUSE),
+    "STOP": KeysCC(CCC.STOP)              , "EJECT": KeysCC(CCC.EJECT),
+    "<<" : KeysCC(CCC.REWIND)             , ">>" :   KeysCC(CCC.FAST_FORWARD),
+    "|<<": KeysCC(CCC.SCAN_PREVIOUS_TRACK), ">>|":   KeysCC(CCC.SCAN_NEXT_TRACK),
+    "VOL-": KeysCC(CCC.VOLUME_DECREMENT)  , "VOL+":  KeysCC(CCC.VOLUME_INCREMENT),
+    "MUTE": KeysCC(CCC.MUTE),
+
+    "1": KeysMain(Keycode.ONE),   "2": KeysMain(Keycode.TWO),   "3": KeysMain(Keycode.THREE),
+    "4": KeysMain(Keycode.FOUR),  "5": KeysMain(Keycode.FIVE),  "6": KeysMain(Keycode.SIX),
+    "7": KeysMain(Keycode.SEVEN), "8": KeysMain(Keycode.EIGHT), "9": KeysMain(Keycode.NINE),
+    "0": KeysMain(Keycode.ZERO) , "SELECT": KeysMain(Keycode.ENTER),
+
+    "NAV_UP":   KeysMain(Keycode.UP_ARROW)  , "NAV_DOWN":  KeysMain(Keycode.DOWN_ARROW),
+    "NAV_LEFT": KeysMain(Keycode.LEFT_ARROW), "NAV_RIGHT": KeysMain(Keycode.RIGHT_ARROW),
+    "BACK":     KeysMain(Keycode.BACKSPACE) , "EXIT":      KeysMain(Keycode.ESCAPE),
+}
+
 #=Options: Numeric input (+enter/select/ok)
 #===============================================================================
 KEYMAP_NUMPAD = {
     "1": KeysMain(Keycode.KEYPAD_ONE),   "2": KeysMain(Keycode.KEYPAD_TWO),   "3": KeysMain(Keycode.KEYPAD_THREE),
     "4": KeysMain(Keycode.KEYPAD_FOUR),  "5": KeysMain(Keycode.KEYPAD_FIVE),  "6": KeysMain(Keycode.KEYPAD_SIX),
     "7": KeysMain(Keycode.KEYPAD_SEVEN), "8": KeysMain(Keycode.KEYPAD_EIGHT), "9": KeysMain(Keycode.KEYPAD_NINE),
-    "0": KeysMain(Keycode.KEYPAD_ZERO), "SELECT": KeysMain(Keycode.KEYPAD_ENTER),
+    "0": KeysMain(Keycode.KEYPAD_ZERO) , "SELECT": KeysMain(Keycode.KEYPAD_ENTER),
 }
-KEYMAP_STD = { #Standard keyboard keys (not numpad)
-    "1": KeysMain(Keycode.ONE),   "2": KeysMain(Keycode.TWO),   "3": KeysMain(Keycode.THREE),
-    "4": KeysMain(Keycode.FOUR),  "5": KeysMain(Keycode.FIVE),  "6": KeysMain(Keycode.SIX),
-    "7": KeysMain(Keycode.SEVEN), "8": KeysMain(Keycode.EIGHT), "9": KeysMain(Keycode.NINE),
-    "0": KeysMain(Keycode.ZERO), "SELECT": KeysMain(Keycode.ENTER),
+if SEND_NUMPAD:
+    KEYMAP.update(KEYMAP_NUMPAD) #Overwrite with numpad characters
+
+
+#=Options: FF/REW and skip buttons
+#===============================================================================
+#Special option for remotes supporting ONLY FF/REW (not next/previous track):
+SKIPCHAR = "|" if SEND_SKIP_IF_FFREW_ONLY else "" #Switch to "skip" keys if requested
+KEYMAP_FRONLY = {
+    "<<" : (KEYMAP[SKIPCHAR+"<<"]),
+    ">>" : (KEYMAP[">>"+SKIPCHAR]),
 }
-KEYMAP = KEYMAP_NUMPAD if USE_NUMPAD else KEYMAP_STD
 
 
 #=Main config
 #===============================================================================
 rx = IRRx(rx_pin)
 SIGNAL_MAP_ADAFRUIT_389_CCC = { #Mapping for Adafruit 389 Mini Remote Control
-    0xFF629D: KeysCC(CCC.PLAY_PAUSE), 0xFFC23D: KeysCC(CCC.STOP),
-    0xFFA25D: KeysCC(CCC.VOLUME_DECREMENT), 0xFFE21D: KeysCC(CCC.VOLUME_INCREMENT),
-    0xFF22DD: KeysCC(CCC.MUTE), #"setup" button (options: ESCAPE, KEYPAD_NUMLOCK, MUTE?)
+    0xFF629D: KEYMAP["PLAY"], 0xFFC23D: KEYMAP["STOP"],
+    0xFFA25D: KEYMAP["VOL-"], 0xFFE21D: KEYMAP["VOL+"],
+    0xFF22DD: KEYMAP["MUTE"], #"setup" button (options: ESCAPE, KEYPAD_NUMLOCK, MUTE?)
 }
 SIGNAL_MAP_ADAFRUIT_389_EXTRAS = { #Extras (numeric + arrows)
     0xFF30CF: KEYMAP["1"], 0xFF18E7: KEYMAP["2"], 0xFF7A85: KEYMAP["3"],
     0xFF10EF: KEYMAP["4"], 0xFF38C7: KEYMAP["5"], 0xFF5AA5: KEYMAP["6"],
     0xFF42BD: KEYMAP["7"], 0xFF4AB5: KEYMAP["8"], 0xFF52AD: KEYMAP["9"],
     0xFF6897: KEYMAP["0"], 0xFFA857: KEYMAP["SELECT"],
-    0xFF02FD: KeysMain(Keycode.UP_ARROW), 0xFF9867: KeysMain(Keycode.DOWN_ARROW),
-    0xFFE01F: KeysMain(Keycode.LEFT_ARROW), 0xFF906F: KeysMain(Keycode.RIGHT_ARROW),
-    0xFFB04F: KeysMain(Keycode.BACKSPACE), #"return"
+    0xFF02FD: KEYMAP["NAV_UP"], 0xFF9867: KEYMAP["NAV_DOWN"],
+    0xFFE01F: KEYMAP["NAV_LEFT"], 0xFF906F: KEYMAP["NAV_RIGHT"],
+    0xFFB04F: KEYMAP["EXIT"], #"return"
 }
 
 SIGNAL_MAP_LG_CCC = { #Mapping for some LG IR remote (NEC protocol)
-    0x20DF0DF2: KeysCC(CCC.PLAY_PAUSE), 0x20DF5DA2: KeysCC(CCC.PLAY_PAUSE), #"play" & "pause" buttons
-    0x20DF8D72: KeysCC(CCC.STOP),
-    0x20DFF10E: KeysCC(CCC.SCAN_PREVIOUS_TRACK), 0x20DF718E: KeysCC(CCC.SCAN_NEXT_TRACK),
-    0x20DFC03F: KeysCC(CCC.VOLUME_DECREMENT), 0x20DF40BF: KeysCC(CCC.VOLUME_INCREMENT),
-    0x20DF906F: KeysCC(CCC.MUTE),
+    0x20DF0DF2: KEYMAP["PLAY"], 0x20DF5DA2: KEYMAP["PAUSE"], 0x20DF8D72: KEYMAP["STOP"],
+    0x20DFF10E: KEYMAP_FRONLY["<<"], 0x20DF718E: KEYMAP_FRONLY[">>"],
+    0x20DFC03F: KEYMAP["VOL-"], 0x20DF40BF: KEYMAP["VOL+"],
+    0x20DF906F: KEYMAP["MUTE"],
 }
 #Not likely to want "extras" to be sent to your PC:
 SIGNAL_MAP_LG_EXTRAS = { #Mapping for some LG IR remote (NEC protocol)
@@ -74,9 +100,9 @@ SIGNAL_MAP_LG_EXTRAS = { #Mapping for some LG IR remote (NEC protocol)
     0x20DF28D7: KEYMAP["4"], 0x20DFA857: KEYMAP["5"], 0x20DF6897: KEYMAP["6"],
     0x20DFE817: KEYMAP["7"], 0x20DF18E7: KEYMAP["8"], 0x20DF9867: KEYMAP["9"],
     0x20DF08F7: KEYMAP["0"], 0x20DF22DD: KEYMAP["SELECT"],
-    0x20DF02FD: KeysMain(Keycode.UP_ARROW), 0x20DF827D: KeysMain(Keycode.DOWN_ARROW),
-    0x20DFE01F: KeysMain(Keycode.LEFT_ARROW), 0x20DF609F: KeysMain(Keycode.RIGHT_ARROW),
-    0x20DF14EB: KeysMain(Keycode.BACKSPACE), 0x20DFDA25: KeysMain(Keycode.ESCAPE), #"back" & "exit"
+    0x20DF02FD: KEYMAP["NAV_UP"], 0x20DF827D: KEYMAP["NAV_DOWN"],
+    0x20DFE01F: KEYMAP["NAV_LEFT"], 0x20DF609F: KEYMAP["NAV_RIGHT"],
+    0x20DF14EB: KEYMAP["BACK"], 0x20DFDA25: KEYMAP["EXIT"],
 }
 
 #Respond to both remotes (NOTE: cannot have overlapping codes)
