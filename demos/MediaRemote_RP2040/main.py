515de14c8fdb63f519cbb80e9ccf751bfaf35259
Alain <alainman.krh@gmail.com>
2024-10-23 21:35:00 -0400

    Not as efficient - but provides better output.

--- demos/MediaRemote_RP2040/main.py
+++ demos/MediaRemote_RP2040/main.py
@@ -17,6 +17,7 @@ import board
 #=Option slect
 #===============================================================================
 SEND_CONSUMERCONTROL_ONLY = False #Support basic media keys only (remote will not "type text")
+#==> (Not likely to want "extras" to be sent to your PC (numeric values + arrows))
 SEND_SKIP_IF_FFREW_ONLY = True #Send "skip" keys on remotes that only support FF/RW (no proper skip buttons)
 SEND_NUMPAD = True #Send numpad keys vs standard numbers/enter key (SEND_CONSUMERCONTROL_ONLY must =False)
 
@@ -24,12 +25,11 @@ SEND_NUMPAD = True #Send numpad keys vs standard numbers/enter key (SEND_CONSUME
 #=Platform/build-dependent config (Raspberry Pi Pico RP2040)
 #===============================================================================
 rx_pin = board.GP28
-#Not likely to want "extras" to be sent to your PC (numeric values + arrows):
+#rx_pin = board.D9 #Variant: KB2040 "Kee Boar"-based
 
 
 #=Base keymap
 #===============================================================================
-FRAME_FORWARD = 0xC6
 KEYMAP = {
     "PLAY": KeysCC(CCC.PLAY_PAUSE)        , "PAUSE": KeysCC(CCC.PLAY_PAUSE),
     "STOP": KeysCC(CCC.STOP)              , "EJECT": KeysCC(CCC.EJECT),
@@ -64,45 +64,43 @@ if SEND_NUMPAD:
 #===============================================================================
 #Special option for remotes supporting ONLY FF/REW (not next/previous track):
 SKIPCHAR = "|" if SEND_SKIP_IF_FFREW_ONLY else "" #Switch to "skip" keys if requested
-KEYMAP_FRONLY = {
-    "<<" : (KEYMAP[SKIPCHAR+"<<"]),
-    ">>" : (KEYMAP[">>"+SKIPCHAR]),
-}
+KEYMAP["<<-only"] = KEYMAP[SKIPCHAR+"<<"]
+KEYMAP[">>-only"] = KEYMAP[">>"+SKIPCHAR]
 
 
 #=Main config
 #===============================================================================
 rx = IRRx(rx_pin)
 SIGNAL_MAP_ADAFRUIT_389_CCC = { #Mapping for Adafruit 389 Mini Remote Control
-    0xFF629D: KEYMAP["PLAY"], 0xFFC23D: KEYMAP["STOP"],
-    0xFFA25D: KEYMAP["VOL-"], 0xFFE21D: KEYMAP["VOL+"],
-    0xFF22DD: KEYMAP["MUTE"], #"setup" button (options: ESCAPE, KEYPAD_NUMLOCK, MUTE?)
+    0xFF629D: "PLAY", 0xFFC23D: "STOP",
+    0xFFA25D: "VOL-", 0xFFE21D: "VOL+",
+    0xFF22DD: "MUTE", #"setup" button (options: ESCAPE, KEYPAD_NUMLOCK, MUTE?)
 }
 SIGNAL_MAP_ADAFRUIT_389_EXTRAS = { #Extras (numeric + arrows)
-    0xFF30CF: KEYMAP["1"], 0xFF18E7: KEYMAP["2"], 0xFF7A85: KEYMAP["3"],
-    0xFF10EF: KEYMAP["4"], 0xFF38C7: KEYMAP["5"], 0xFF5AA5: KEYMAP["6"],
-    0xFF42BD: KEYMAP["7"], 0xFF4AB5: KEYMAP["8"], 0xFF52AD: KEYMAP["9"],
-    0xFF6897: KEYMAP["0"], 0xFFA857: KEYMAP["SELECT"],
-    0xFF02FD: KEYMAP["NAV_UP"], 0xFF9867: KEYMAP["NAV_DOWN"],
-    0xFFE01F: KEYMAP["NAV_LEFT"], 0xFF906F: KEYMAP["NAV_RIGHT"],
-    0xFFB04F: KEYMAP["EXIT"], #"return"
+    0xFF30CF: "1", 0xFF18E7: "2", 0xFF7A85: "3",
+    0xFF10EF: "4", 0xFF38C7: "5", 0xFF5AA5: "6",
+    0xFF42BD: "7", 0xFF4AB5: "8", 0xFF52AD: "9",
+    0xFF6897: "0", 0xFFA857: "SELECT",
+    0xFF02FD: "NAV_UP", 0xFF9867: "NAV_DOWN",
+    0xFFE01F: "NAV_LEFT", 0xFF906F: "NAV_RIGHT",
+    0xFFB04F: "EXIT", #"return"
 }
 
 SIGNAL_MAP_LG_CCC = { #Mapping for some LG IR remote (NEC protocol)
-    0x20DF0DF2: KEYMAP["PLAY"], 0x20DF5DA2: KEYMAP["PAUSE"], 0x20DF8D72: KEYMAP["STOP"],
-    0x20DFF10E: KEYMAP_FRONLY["<<"], 0x20DF718E: KEYMAP_FRONLY[">>"],
-    0x20DFC03F: KEYMAP["VOL-"], 0x20DF40BF: KEYMAP["VOL+"],
-    0x20DF906F: KEYMAP["MUTE"],
+    0x20DF0DF2: "PLAY", 0x20DF5DA2: "PAUSE", 0x20DF8D72: "STOP",
+    0x20DFF10E: "<<-only", 0x20DF718E: ">>-only",
+    0x20DFC03F: "VOL-", 0x20DF40BF: "VOL+",
+    0x20DF906F: "MUTE",
 }
 #Not likely to want "extras" to be sent to your PC:
 SIGNAL_MAP_LG_EXTRAS = { #Mapping for some LG IR remote (NEC protocol)
-    0x20DF8877: KEYMAP["1"], 0x20DF48B7: KEYMAP["2"], 0x20DFC837: KEYMAP["3"],
-    0x20DF28D7: KEYMAP["4"], 0x20DFA857: KEYMAP["5"], 0x20DF6897: KEYMAP["6"],
-    0x20DFE817: KEYMAP["7"], 0x20DF18E7: KEYMAP["8"], 0x20DF9867: KEYMAP["9"],
-    0x20DF08F7: KEYMAP["0"], 0x20DF22DD: KEYMAP["SELECT"],
-    0x20DF02FD: KEYMAP["NAV_UP"], 0x20DF827D: KEYMAP["NAV_DOWN"],
-    0x20DFE01F: KEYMAP["NAV_LEFT"], 0x20DF609F: KEYMAP["NAV_RIGHT"],
-    0x20DF14EB: KEYMAP["BACK"], 0x20DFDA25: KEYMAP["EXIT"],
+    0x20DF8877: "1", 0x20DF48B7: "2", 0x20DFC837: "3",
+    0x20DF28D7: "4", 0x20DFA857: "5", 0x20DF6897: "6",
+    0x20DFE817: "7", 0x20DF18E7: "8", 0x20DF9867: "9",
+    0x20DF08F7: "0", 0x20DF22DD: "SELECT",
+    0x20DF02FD: "NAV_UP", 0x20DF827D: "NAV_DOWN",
+    0x20DFE01F: "NAV_LEFT", 0x20DF609F: "NAV_RIGHT",
+    0x20DF14EB: "BACK", 0x20DFDA25: "EXIT",
 }
 
 #Respond to both remotes (NOTE: cannot have overlapping codes)
@@ -118,20 +116,23 @@ if not SEND_CONSUMERCONTROL_ONLY:
 #===============================================================================
 class IRDetect(EasyRx):
     def handle_press(self, msg:IRMsg32):
-        key = SIGNAL_MAP.get(msg.bits, None)
-        if key is None:
-            print("Unknown message:", msg.str_hex())
+        sig = SIGNAL_MAP.get(msg.bits, None)
+        IRcodestr = msg.str_hex()
+        if sig is None:
+            print("Unknown message:", IRcodestr)
             return
-        print("New message:", msg.str_hex())
+        print(f"New message: {IRcodestr} ({sig})")
+        key = KEYMAP[sig] #A key that can be sent out through USB-HID interface
         key.press()
 
     def handle_hold(self, msg:IRMsg32):
-        print(f"Repeat!")
+        print(f"Repeat!") #Doesn't matter what msg is - USB-HID key still held down.
 
     def handle_release(self, msg:IRMsg32):
-        key = SIGNAL_MAP.get(msg.bits, None)
-        if key is None:
+        sig = SIGNAL_MAP.get(msg.bits, None)
+        if sig is None:
             return
+        key = KEYMAP[sig] #A key that can be sent out through USB-HID interface
         key.release()
 
 irdetect = IRDetect(rx, PDE.DecoderNEC(), PDE.DecoderNECRPT(), msgRPT=PDE.IRMSG32_NECRPT)
